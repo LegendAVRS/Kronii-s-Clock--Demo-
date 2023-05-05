6235a3d058ff54262308d83284ceb226df457120
LegendAVRS <61302586+LegendAVRS@users.noreply.github.com>
2023-05-05 15:03:02 +0700

    Added setting bar, sfx, bgm

--- Clock.js
+++ Clock.js
@@ -4,6 +4,11 @@ class Clock {
     this.minute = minute;
     this.second = second;
 
+    this.miliSecond = 0;
+
+    this.oldMin = this.minute;
+    this.oldHour = this.hour;
+
     // Sets update period to n miliseconds
     this.interval = 1000;
 
@@ -11,6 +16,9 @@ class Clock {
     this.clock = clock;
     this.isRunning = true;
     this.clock.addEventListener("click", this.activateClock);
+
+    this.setMode = false;
+    this.smoothMode = true;
 
     this.clock.childNodes.forEach((element) => {
       switch (element.id) {
@@ -31,52 +39,94 @@ class Clock {
           break;
       }
     });
+
+    this.timeLabel = document.querySelector("#time-label-box");
+
+    this.sfxOn = false;
+    this.isTickTurn = true;
+    this.tickSFX = new Audio("tick.mp3");
+    this.tockSFX = new Audio("tock.mp3");
+    this.tockSFX.volume = this.tickSFX.volume = 0.3;
   }
 
-  // Pauses or continues the clock
-  activateClock = (event) => {
-    if (this.isRunning) {
-      this.stopClock();
-    } else {
-      this.continueClock();
-    }
-
-    this.isRunning = !this.isRunning;
-  };
-
-  stopClock = () => {
-    this.hourHand.style.animationPlayState = "paused";
-    this.minuteHand.style.animationPlayState = "paused";
-    this.secondHand.style.animationPlayState = "paused";
-  };
-
-  continueClock = () => {
-    this.hourHand.style.animationPlayState = "running";
-    this.minuteHand.style.animationPlayState = "running";
-    this.secondHand.style.animationPlayState = "running";
-  };
-
-  setCurrentHands = (cssVarFirst, cssVarSecond, angle) => {
-    document.documentElement.style.setProperty(cssVarFirst, `${angle}deg`);
-    document.documentElement.style.setProperty(
-      cssVarSecond,
-      `${angle + 360}deg`
-    );
-  };
-
-  rotateHands = (element, angle) => {
-    element.style.setProperty('--angle', `${angle}deg`);
+  // Rotates clock hand
+  rotateHand = (element, angle) => {
+    element.style.setProperty("--angle", `${angle}deg`);
   };
 
   // Updates the current time after a set interval
-  updateTime =  () => {
-      let currentDate = new Date();
-      this.second = currentDate.getSeconds() + currentDate.getMilliseconds() / 1000;
-      this.minute = currentDate.getMinutes() + this.second / 60;
-      this.hour = currentDate.getHours() + this.minute / 60;
-      this.rotateHands(this.secondHand, this.#convertSecondToAngle(this.second));
-      this.rotateHands(this.minuteHand, this.#convertMinuteToAngle(this.minute));
-      this.rotateHands(this.hourHand, this.#convertHourToAngle(this.hour)); 
+  updateClockCurrentTime = () => {
+    let currentDate = new Date();
+    this.second =
+      currentDate.getSeconds() + currentDate.getMilliseconds() / 1000;
+    this.minute = currentDate.getMinutes() + this.second / 60;
+    this.hour = currentDate.getHours() + this.minute / 60;
+
+    this.rotateHand(this.secondHand, this.#convertSecondToAngle(this.second));
+    this.rotateHand(this.minuteHand, this.#convertMinuteToAngle(this.minute));
+    this.rotateHand(this.hourHand, this.#convertHourToAngle(this.hour));
+
+    this.oldTimeLabelText = this.timeLabel.textContent;
+    this.timeLabel.textContent = this.timeToString();
+    if (this.timeLabel.textContent !== this.oldTimeLabelText && this.sfxOn) {
+      this.playSFX();
+    }
+  };
+
+  playSFX = () => {
+    if (this.isTickTurn) this.tickSFX.play();
+    else this.tockSFX.play();
+    this.isTickTurn = !this.isTickTurn;
+  }
+
+  timeToString = () => {
+    let secondString =
+      this.second >= 10 ? Math.floor(this.second).toString() : `0${Math.floor(this.second).toString()}`;
+    let minuteString =
+      this.minute >= 10 ? Math.floor(this.minute).toString() : `0${Math.floor(this.minute).toString()}`;
+    let hourString =
+      this.hour >= 10 ? Math.floor(this.hour).toString() : `0${Math.floor(this.hour).toString()}`;
+
+    return `${hourString}:${minuteString}:${secondString}`;
+  };
+
+  setTime = (hour, minute, second) => {
+    this.hour = hour;
+    this.minute = minute;
+    this.second = second;
+
+    this.origHour = hour;
+    this.origMinute = minute;
+    this.origSecond = second;
+
+    this.origDate = new Date();
+
+    this.miliSecond = 0;
+  };
+
+  updateClockSetTime = () => {
+    let tempDate = new Date();
+    let timeDif = tempDate - this.origDate;
+
+    this.second = this.origSecond + timeDif / 1000;
+    this.minute = this.origMinute + this.second / 60;
+    this.hour = this.origHour + this.minute / 60;
+
+    if (this.second >= 60) {
+      this.second = 0;
+      this.origMinute++;
+    }
+
+    if (this.minute >= 60) {
+      this.minute = 0;
+      this.origHour++;
+    }
+
+    this.hour %= 24;
+
+    this.rotateHand(this.secondHand, this.#convertSecondToAngle(this.second));
+    this.rotateHand(this.minuteHand, this.#convertMinuteToAngle(this.minute));
+    this.rotateHand(this.hourHand, this.#convertHourToAngle(this.hour));
   };
 
   // Returns the angle of the respective time
@@ -91,14 +141,6 @@ class Clock {
   #convertHourToAngle = (hours) => {
     return hours * (360 / 12);
   };
-
-  #secondToMinuteOffset = (seconds) => {
-    return (seconds % 60) * (6 / 60);
-  }
-
-  #minuteToHourOffset = (minutes) => {
-    return (minutes % 60) * (6 / 12);
-  }
 }
 
 export default Clock;
--- script.js
+++ script.js
@@ -1,8 +1,106 @@
-import Clock from "./Clock.js"
+import Clock from "./Clock.js";
 
 let myClock = new Clock(1, 1, 30, document.querySelector(".clock"));
 
-setInterval(myClock.updateTime, 1);
-myClock.updateTime();
-// myClock.stopClock();
+let intervalRate = 10;
+let currentTimeInterval = setInterval(myClock.updateClockCurrentTime, intervalRate);
+let sfxInterval = null;
+let setTimeInterval = null; 
 
+
+myClock.updateClockCurrentTime();
+
+let optionBar = document.querySelector(".setting-bar");
+let navButton = document.querySelector("#setting-icon");
+let inputHour = document.querySelector("#input-hour");
+let inputMinute = document.querySelector("#input-minute");
+let inputSecond = document.querySelector("#input-second");
+let setButton = document.querySelector("#set-button");
+let pauseButton = document.querySelector("#pause-button");
+let resetButton = document.querySelector("#reset-button");
+let bgmCheck = document.querySelector("#check-bgm");
+let smoothCheck = document.querySelector("#check-smooth");
+let sfxCheck = document.querySelector("#check-sfx");
+let showCheck = document.querySelector("#check-show");
+let timeBox = document.querySelector("#time-label-box");
+
+function closeOptionBar(event) {
+  optionBar.classList.toggle("close");
+}
+
+let setTime = (event) => {
+  clearInterval(currentTimeInterval);
+  if (setTimeInterval !== null)  clearInterval(setTimeInterval);
+  myClock.setTime(parseInt(inputHour.value), parseInt(inputMinute.value), parseInt(inputSecond.value));
+  setTimeInterval = setInterval(myClock.updateClockSetTime, intervalRate);
+};
+
+let isPaused = false;
+let pauseTime = (event) => {
+  if (isPaused) {
+    if (!myClock.setMode) currentTimeInterval = setInterval(myClock.updateClockCurrentTime,intervalRate);
+    else setTimeInterval = setInterval(myClock.updateClockSetTime,intervalRate);
+    pauseButton.textContent = "Pause";
+  } else {
+    clearInterval(currentTimeInterval);
+    clearInterval(setTimeInterval);
+    pauseButton.textContent = "Continue";
+  }
+  isPaused = !isPaused;
+};
+
+let resetTime = (event) => {
+  clearInterval(setTimeInterval);
+  clearInterval(currentTimeInterval);
+  currentTimeInterval = setInterval(myClock.updateClockCurrentTime,intervalRate);
+};
+
+let bgm = new Audio("bgm.mp3");
+bgm.loop = true;
+let toggleMusic = (event) => {
+    if (bgmCheck.checked) {
+        bgm.play();
+    }
+    else {
+        bgm.pause();
+    }
+
+}
+
+let toggleSFX = (event) => {
+  myClock.sfxOn = !myClock.sfxOn;
+}
+
+let toggleSmooth = (event) => {
+  if (smoothCheck.checked) {
+    intervalRate = 10;
+  }
+  else {
+    intervalRate = 1000;
+  }
+  if (myClock.setMode) {
+    clearInterval(setTimeInterval);
+    setTimeInterval = setInterval(myClock.updateClockSetTime, intervalRate);
+  }
+  else {
+    clearInterval(currentTimeInterval);
+    currentTimeInterval = setInterval(myClock.updateClockCurrentTime, intervalRate);
+  }
+}
+
+let toggleShow = (event) => {
+  timeBox.classList.toggle("hide");
+}
+
+smoothCheck.checked = true;
+showCheck.checked = true;
+
+
+navButton.addEventListener("click", closeOptionBar);
+pauseButton.addEventListener("click", pauseTime);
+setButton.addEventListener("click", setTime);
+resetButton.addEventListener("click", resetTime);
+bgmCheck.addEventListener("click", toggleMusic);
+smoothCheck.addEventListener("click", toggleSmooth);
+showCheck.addEventListener("click", toggleShow);
+sfxCheck.addEventListener("click", toggleSFX);
